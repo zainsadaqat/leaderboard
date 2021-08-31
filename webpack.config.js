3d91d63f3f0b0d43c1667734a22c1f6a4bbfb622
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-01 02:17:55 +0500

    Add babel-loader to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+    ],
+  },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   output: {
     filename: 'main.js',
