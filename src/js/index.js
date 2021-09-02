b3d3228e4aac3ab9cbc86641494d9212554c8d08
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-03 02:31:15 +0500

    Submit form

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ require('@babel/polyfill');
 
 const leaderboard = document.querySelector('.leaderboard');
 const refresh = document.querySelector('#refresh');
-const submit = document.querySelector('#submit');
+const form = document.querySelector('#form');
 const userInput = document.querySelector('#user');
 const scoreInput = document.querySelector('#score');
 const message = `<h6 class="text-center">Fetch Data by clicking refresh button</h6>`;
@@ -17,11 +17,7 @@ refresh.addEventListener('click', () => {
   leaderboard.innerHTML = ``;
 });
 
-submit.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
   postData(userInput.value, scoreInput.value);
+  e.target.reset();
 });
-
-const clearFields = () => {
-  userInput.value = '';
-  scoreInput.value = '';
-};
