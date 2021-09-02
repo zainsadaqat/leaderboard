a72ad4e0dc12603e3da4e1bcca056949d2c30f6a
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-02 06:38:41 +0500

    Fetch scores list from api using async await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+require('@babel/polyfill');
+
+const URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/`;
+
+const scoresList = async () => {
+  const response = await fetch(URL);
+  const jsonResponse = await response.json();
+  const results = jsonResponse.result;
+  results.forEach((result) => {
+    const { user, score } = result;
+  });
+};
+
+scoresList();
+
+// Create template of list and append them to the ul
+// create modular functions
