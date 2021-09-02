7ff02e6270872fe87a275416caad5b04480ad242
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-03 03:53:21 +0500

    Fix eslint issues

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,15 @@ import postData from './post-data.js';
 
 require('@babel/polyfill');
 
-const leaderboard = document.querySelector('.leaderboard');
 const refresh = document.querySelector('#refresh');
 const form = document.querySelector('#form');
 const userInput = document.querySelector('#user');
 const scoreInput = document.querySelector('#score');
-const message =
-  '<h6 class="text-center">Fetch Data by clicking refresh button</h6>';
 
 scoresList();
 
 refresh.addEventListener('click', () => {
   scoresList();
-  leaderboard.innerHTML = '';
 });
 
 form.addEventListener('submit', (e) => {
