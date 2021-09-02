314cea11f8cddcb68af5809721e26c14e2e642f7
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-02 20:52:16 +0500

    Add event listener for submit button

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,24 @@
 import '../css/style.css';
 import scoresList from './get-data';
+import postData from './post-data';
 require('@babel/polyfill');
 
 const leaderboard = document.querySelector('.leaderboard');
-const message = `<h6 class="text-center">Fetch Data by clicking refresh button</h6>`;
-leaderboard.innerHTML = message;
 const refresh = document.querySelector('#refresh');
+const submit = document.querySelector('#submit');
+const userInput = document.querySelector('#user');
+const scoreInput = document.querySelector('#score');
+const message = `<h6 class="text-center">Fetch Data by clicking refresh button</h6>`;
+
+leaderboard.innerHTML = message;
+
 refresh.addEventListener('click', () => {
   scoresList();
   leaderboard.innerHTML = ``;
+});
+
+submit.addEventListener('submit', (e) => {
+  if (userInput === '' || scoreInput === '') return;
+
+  postData(userInput, scoreInput);
 });
