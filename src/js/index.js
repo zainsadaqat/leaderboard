8d8d5cc107ba06a929b53b3e761d99f98016d29f
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-02 19:46:27 +0500

    Implement refresh button

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,11 @@ import '../css/style.css';
 import scoresList from './get-data';
 require('@babel/polyfill');
 
-scoresList();
+const leaderboard = document.querySelector('.leaderboard');
+const message = `<h6 class="text-center">Fetch Data by clicking refresh button</h6>`;
+leaderboard.innerHTML = message;
+const refresh = document.querySelector('#refresh');
+refresh.addEventListener('click', () => {
+  scoresList();
+  leaderboard.innerHTML = ``;
+});
