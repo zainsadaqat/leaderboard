56658fd8ff4dc87e7bab31c2edd7872c8666d63a
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-09-03 03:33:06 +0500

    populate the leaderboard with API data whenever the page loads or reloads

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,10 @@ const refresh = document.querySelector('#refresh');
 const form = document.querySelector('#form');
 const userInput = document.querySelector('#user');
 const scoreInput = document.querySelector('#score');
-const message = '<h6 class="text-center">Fetch Data by clicking refresh button</h6>';
+const message =
+  '<h6 class="text-center">Fetch Data by clicking refresh button</h6>';
 
-leaderboard.innerHTML = message;
+scoresList();
 
 refresh.addEventListener('click', () => {
   scoresList();
